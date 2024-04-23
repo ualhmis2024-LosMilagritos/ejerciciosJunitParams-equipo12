88da81fa6841f36d102856ced6a68580edf643d1
pms863 <114874858+pms863@users.noreply.github.com>
2024-04-23 20:52:09 +0200

    ejercicio 4 csv

--- sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio4Test.java
+++ sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio4Test.java
@@ -10,19 +10,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class Ejercicio4Test {
 
+
+	@CsvFileSource(resources = "ejercicio4.csv", delimiter = ';')
 	@ParameterizedTest(name = "Mezclar {0} con {1}")
-//	@CsvFileSource(resources = "ejercicio4.csv", delimiter = ';')
-	@CsvSource({
-		"'','',''",
-		"'','2,4,6,8,10','2,4,6,8,10'",
-		"'1,3,5,7,9','','1,3,5,7,9'",
-		"'1,3,15,7,9','2,4,6,8,10','1,2,3,4,6,7,8,9,10,15'",
-		"'3,1,5,7,9','3,4,5,8,10','1,3,3,4,5,5,7,8,9,10'",
-		"'1.5,2.5,3.5','4.5,5.5','1.5,2.5,3.5,4.5,5.5'",
-		"'azul,celeste,negro','blanco,rojo','azul,blanco,celeste,negro,rojo'"
-	})
-
-
 	void testMezclarArraysOrdenados(String array1, String array2, String expected) {
 		Ejercicio4 c = new Ejercicio4();
 		Comparable[] intArray1 = parseIntArray(array1);
